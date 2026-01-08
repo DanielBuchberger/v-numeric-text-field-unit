ae77582b57c2664625a07801343922a8d8e673e4
Daniel Buchberger <daniel.buchberger@enrag.at>
2026-01-08 15:08:02 +0100

    add further default unit settings

--- src/types/unitsettings.ts
+++ src/types/unitsettings.ts
@@ -1,4 +1,4 @@
-import type { QuantityType, Unit } from '../plugins/unitConverter/types';
+import type { QuantityType, Unit } from '@/plugins/unitConverter/types';
 
 export interface NumericTextFieldUnitSettings<T extends QuantityType> {
   type: T;
@@ -25,7 +25,6 @@ export const settingsLength: NumericTextFieldUnitSettings<'length'> = {
     { unit: 'mm', precision: 1 },
   ],
 };
-
 export const settingsMassFlow: NumericTextFieldUnitSettings<'massflow'> = {
   type: 'massflow',
   label: 'Massflow',
@@ -36,16 +35,34 @@ export const settingsMassFlow: NumericTextFieldUnitSettings<'massflow'> = {
     { unit: 'kg/h', precision: 2 },
   ],
 };
-
 export const settingsVolumeFlow: NumericTextFieldUnitSettings<'volumeflow'> = {
   type: 'volumeflow',
   label: 'Volumeflow',
   units: [
-    { unit: 'm³/h', precision: 2 },
-    { unit: 'm³/s', precision: 2 },
+    { unit: 'm\u00B3/h', precision: 2 },
+    { unit: 'm\u00B3/s', precision: 2 },
   ],
 };
-
+export const settingsNormVolumeFlow: NumericTextFieldUnitSettings<'volumeflow'> =
+  {
+    type: 'volumeflow',
+    label: 'Norm Volumeflow',
+    currentUnit: { unit: 'Nm\u00B3/h', precision: 2 },
+    baseUnit: 'Nm\u00B3/s',
+    units: [
+      { unit: 'Nm\u00B3/h', precision: 2 },
+      { unit: 'Nm\u00B3/s', precision: 2 },
+    ],
+  };
+export const settingsDensity: NumericTextFieldUnitSettings<'density'> = {
+  type: 'density',
+  label: 'Density',
+  units: [
+    { unit: 'kg/m\u00B3', precision: 2 },
+    { unit: 'g/m\u00B3', precision: 2 },
+    { unit: 'g/cm\u00B3', precision: 2 },
+  ],
+};
 export const settingsPressure: NumericTextFieldUnitSettings<'pressure'> = {
   type: 'pressure',
   label: 'Pressure',
@@ -58,24 +75,62 @@ export const settingsPressure: NumericTextFieldUnitSettings<'pressure'> = {
     { unit: 'atm', precision: 2 },
   ],
 };
-
 export const settingsTemperature: NumericTextFieldUnitSettings<'temperature'> =
   {
     type: 'temperature',
     label: 'Temperature',
     units: [
-      { unit: '°C', precision: 2 },
+      { unit: '\u00B0C', precision: 2 },
       { unit: 'K', precision: 2 },
-      { unit: '°F', precision: 2 },
+      { unit: '\u00B0F', precision: 2 },
+    ],
+  };
+export const settingsSpecificEntropy: NumericTextFieldUnitSettings<'specificEntropy'> =
+  {
+    type: 'specificEntropy',
+    label: 'Specific Entropy',
+    units: [
+      { unit: 'J/kgK', precision: 2 },
+      { unit: 'kJ/kgK', precision: 2 },
+      { unit: 'MJ/kgK', precision: 2 },
+    ],
+  };
+export const settingsTime: NumericTextFieldUnitSettings<'time'> = {
+  type: 'time',
+  label: 'Time',
+  units: [{ unit: 's', precision: 0 }],
+};
+
+export const settingsEnergyFlow: NumericTextFieldUnitSettings<'energyFlow'> = {
+  type: 'energyFlow',
+  label: 'Energy Flow',
+  units: [
+    { unit: 'W', precision: 0 },
+    { unit: 'kW', precision: 0 },
+    { unit: 'MW', precision: 0 },
+  ],
+};
+export const settingsThermalConductivity: NumericTextFieldUnitSettings<'thermalConductivity'> =
+  {
+    type: 'thermalConductivity',
+    label: 'Thermal Conductivity',
+    units: [
+      { unit: 'W/(m*K)', precision: 0 },
+      { unit: 'kW/(m*K)', precision: 0 },
     ],
   };
 
-export const settingsDensity: NumericTextFieldUnitSettings<'density'> = {
-  type: 'density',
-  label: 'Density',
+export const settingsArea: NumericTextFieldUnitSettings<'area'> = {
+  type: 'area',
+  label: 'Area',
   units: [
-    { unit: 'kg/m³', precision: 2 },
-    { unit: 'g/m³', precision: 2 },
-    { unit: 'g/cm³', precision: 2 },
+    { unit: 'mm²', precision: 0 },
+    { unit: 'cm²', precision: 0 },
+    { unit: 'm²', precision: 0 },
   ],
+};
+export const settingsRatio: NumericTextFieldUnitSettings<'ratio'> = {
+  type: 'ratio',
+  label: 'Ratio',
+  units: [{ unit: '%', precision: 4 }],
 };
