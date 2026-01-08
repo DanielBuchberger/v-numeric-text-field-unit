3236ae6ce6992186c96dfe6ba28634576501b224
Daniel Buchberger <daniel.buchberger@enrag.at>
2026-01-08 14:55:18 +0100

    export further types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export { default as VNumericTextField } from './components/VNumericTextField.vue';
 export { default as VNumericTextFieldUnit } from './components/VNumericTextFieldUnit.vue';
 export * from './plugins';
+export type {
+  NumericTextFieldUnitSettings,
+  UnitSettings,
+} from './types/unitsettings';
