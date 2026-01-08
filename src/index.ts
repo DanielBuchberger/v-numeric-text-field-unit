ffdcfdb58356af390a0d6d8aa8f445bb3c270c06
Daniel Buchberger <daniel.buchberger@enrag.at>
2026-01-08 15:05:05 +0100

    Export all settings from unitsettings

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export { default as VNumericTextField } from './components/VNumericTextField.vue';
 export { default as VNumericTextFieldUnit } from './components/VNumericTextFieldUnit.vue';
 export * from './plugins';
-export type {
-  NumericTextFieldUnitSettings,
-  UnitSettings,
-} from './types/unitsettings';
+export * from './types/unitsettings';
